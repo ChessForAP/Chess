b5a98524de5427ca400820452a95f21b52d6268f
Wibowyz <36877037+Wibowyz@users.noreply.github.com>
2018-03-06 14:52:59 +0800

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,15 +1,26 @@
 public class Knight extends Chess
 {
-   private int x;
-   private int y;
-   private String color;
-   public Knight(String color)
-   {
-       this.color= color;
-       super.add();
-   }
-   public String toString()
-   {
+    private int x;
+    private int y;
+    private String color;
+    private Knight(String color)
+    {
+        this.color=color;
+        super.num+=1;
+    }
+    public String toString()
+    {
         return color+"Kn    ";
-   }
+    }
+    public static Knight create(String color)
+    {
+        if(checkNum())
+        {
+            return new Knight(color);
+        }
+        else
+        {
+            throw new Error("more than 32");
+        }
+    }
 }
