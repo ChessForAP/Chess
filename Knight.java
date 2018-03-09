a9c44d4d8b7aa54a2d3c347914e04a6917c25054
deillusion <36877070+deillusion@users.noreply.github.com>
2018-03-09 09:00:21 +0800

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -23,4 +23,22 @@ public class Knight extends Chess
             throw new Error("more than 32");
         }
     }
+    public void setPosition(int x,int y){
+        if(0<=x && x<=7 && y<=7 && 0<=y){
+            if(Math.abs(this.x-x)==2 && Math.abs(y-this.y)==1){
+                    this.x=x;
+                    this.y=y;
+            }
+            else if(Math.abs(this.x-x)==1 && Math.abs(y-this.y)==2){
+                    this.x=x;
+                    this.y=y;
+            }
+            else{
+                throw new Error("illegal moving");
+            }
+        }
+        else{
+            throw new Error("out of table");
+        }
+    }
 }
