280c47c8f908c3eba91984933e8a3e4d55f1b831
deillusion <36877070+deillusion@users.noreply.github.com>
2018-03-09 09:17:06 +0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,17 @@
+public class Player
+{
+    private String color;
+    private String name;
+    public Player(String color,String name)
+    {
+        this.color=color;
+        this.name=name;
+    }
+    public String getColor()
+    {
+        return this.color;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ game1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class game1
+{
+    public void game(){
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Please Enter the name of User1 :");
+        Player player1= new Player("W",scanner1.next());
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Please Enter the name of User2 :");
+        Player player2= new Player("B",scanner2.next());
+        Table table=new Table();
+        while(true){
+            table.printOut();
+            move(table,player1);
+            if(table.over(player1.getColor())){
+                System.out.println(player1.getName()+" win");
+                break;
+            }
+            table.printOut();
+            move(table,player2);
+            if(table.over(player2.getColor())){
+                System.out.println(player2.getName()+" win");
+                break;
+            }
+        }
+    }
+    private void move(Table table, Player player1){
+        Scanner condition = new Scanner(System.in);
+        System.out.println(player1.getName()+":");
+        String str=condition.next();
+        table.changePosition(str,player1.getColor());
+    }
+}
