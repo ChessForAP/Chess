b6350ed2d23dd3086e426fbbb4b25def98855f52
deillusion <36877070+deillusion@users.noreply.github.com>
2018-03-09 08:56:51 +0800

    Update King.java

--- King.java
+++ King.java
@@ -22,4 +22,19 @@ public class King extends Chess
             throw new Error("more than 32 ");
         }
    }
+   public void setPosition(int x,int y){
+        if(0<=x && x<=7 && y<=7 && 0<=y){
+            if(Math.abs(this.x-x)==1 || Math.abs(this.y-y)==1){
+                this.x=x;
+                this.y=y;
+                var=false;
+            }
+            else{
+                throw new Error("illegal moving");
+            }
+        }
+        else{
+            throw new Error("out of table");
+        }
+    }
 }
