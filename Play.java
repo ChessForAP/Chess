907101b39678c0857f49a2bcd70717f60827fa4f
Wibowyz <36877037+Wibowyz@users.noreply.github.com>
2018-03-28 10:03:37 +0800

    Add files via upload

--- Play.java
+++ Play.java
@@ -1,7 +1,14 @@
-//upload by Carol Li
+
+/**
+ * Write a description of class Play here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+//upload by Carol Li--Yolanda Su
 public class Play
 {
-    public static void play()
+    public static void play() throws Exception
     {
         new Game();
     }
